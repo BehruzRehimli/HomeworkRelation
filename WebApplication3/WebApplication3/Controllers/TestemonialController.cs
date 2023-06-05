18d4a85ce1f5f2dc3ef1c419baa2683df2dfc482
BehruzRehimli <bahruzkr@code.edu.az>
2023-06-05 13:27:17 +0400

    doneTask

---
+++ WebApplication3/WebApplication3/Controllers/TestemonialController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.DAL;
+
+namespace WebApplication3.Controllers
+{
+    public class TestemonialController : Controller
+    {
+        readonly RelationDbContext _context;
+        public TestemonialController(RelationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TestDetails(int id) {
+            var data=_context.Testimonials.Include(x=>x.Profession).ToList().Find(x=>x.Id==id);
+
+            return View(data);
+        }
+    }
+}
